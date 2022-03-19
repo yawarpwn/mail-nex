71f0b2b6edfa5b6a952e05124269f3f10e47a174
yawarpwn <john.kazz@gmail.com>
2022-03-18 19:23:21 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        'next'
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
---
+++ pages/api/send-email.js
@@ -0,0 +1,49 @@
+const nodemailer = require("nodemailer");
+
+export default function handler (req, res) {
+  const { name, email, message } = req.body;
+
+  const html = `
+    <div>
+      <h2>Informacion</h2>
+      <ul>
+        <li>usuario: ${name}</li>
+        <li>correo: ${email}</li>
+      </ul>
+      <h2>Mensaje:</h2>
+      <p>${message}</p>
+      <ul>
+
+      </ul>
+    </div>
+  `;
+
+  async function nodeMailer() {
+    const transporter = nodemailer.createTransport({
+      host: "mail.tellsenales.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: "ventas@tellsenales.com", // generated ethereal user
+        pass: "Vts4524987$", // generated ethereal password
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: email, // sender address
+      to: "tellsenales@gmail.com", // list of receivers
+      subject: `Mensaje de ${name} `, // Subject line
+      text: "Hello world?", // plain text body
+      html: html, // html body
+    });
+
+    console.log("Message sent", info.messageId);
+    console.log(req.body);
+    res.json("recibido");
+  }
+
+  nodeMailer();
+}
